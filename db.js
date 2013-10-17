bf555df21a130271aab1ee54f6caf4564dafcad3
Brayden Cleary <brayden.cleary@gmail.com>
2013-10-16 23:44:52 -0700

    Refercing db env variable for heroku deployment

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ var pg = require('pg')
 
 module.exports = db = {}
 
-db.url = "postgres://braydencleary@localhost/concussion_tracker"
+db.url = process.env.DATABASE_URL || "postgres://braydencleary@localhost/concussion_tracker"
 
 db.readQuery = function(query) {
   var rs = new ReadableStream({objectMode:true})
